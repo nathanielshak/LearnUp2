04b439844af3437e888bf96d33fd54df76f96897
nathanielshak <nathaniel.shak@gmail.com>
2015-11-18 13:45:14 -0800

    Initial Commit

---
+++ LearnUp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnUp2
+//
+//  Created by Nathaniel Shak on 11/18/15.
+//  Copyright © 2015 Nathaniel Shak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
