138c0c8cd737995ab6a9ad7c7773bdac7d1a8a81
nathanielshak <nathaniel.shak@gmail.com>
2015-11-24 14:53:00 -0800

    added nav bar, back button

--- LearnUp2/HomeScreen.swift
+++ LearnUp2/HomeScreen.swift
@@ -1,17 +1,16 @@
 //
-//  ViewController.swift
+//  HomeScreen.swift
 //  LearnUp2
 //
-//  Created by Nathaniel Shak on 11/18/15.
+//  Created by Nathaniel Shak on 11/24/15.
 //  Copyright © 2015 Nathaniel Shak. All rights reserved.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
+class HomeScreen: UIViewController {
     
     //MARK: properties
-
+    
     
     @IBOutlet weak var myTasksButton: UIView!
     @IBOutlet weak var searchButton: UIView!
@@ -24,17 +23,13 @@ class ViewController: UIViewController {
         searchButton.layer.cornerRadius = 10
         progressButton.layer.cornerRadius = 10
         
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.translucent = true
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
